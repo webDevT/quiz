fc888ff4f4c3fabb5c18f357493f28f6650e5b5d
webDevT <karinadesyatnik27@gmail.com>
2023-11-27 11:38:08 +0200

    updates

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,5 +6,33 @@ $('.quize-btn').click(function(){
   $('.next-btn').addClass('active');
 })
 
+$('.lets-try-button').click(function(){
+  $(".quize__text-content").hide();
+  $('.photo-wrapper').hide();
+  $('.special-offer__content').addClass('no-active');
+  $('.steps-row').show();  
+  $('.step1').show();
+  $('.next-btn').addClass('show');
+})
+
+$('.next-btn--1').click(function(){
+  $('.step1').hide();
+  $('.step2').show();
+  $('.step-line--2').addClass('active');
+})
+
+$('.next-btn--2').click(function(){
+  $('.step2').hide();
+  $('.step3').show();
+  $('.step-line--3').addClass('active');
+})
+
+$('.next-btn--3').click(function(){
+  $('.step3').hide();
+  $('.step4').show();
+  $('.steps-row').hide();
+
+})
+
 });
 
